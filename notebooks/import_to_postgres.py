b74abde6af2afc53273949903db51538a9967f3e
Beaver-Kris <kris.bobrovskaya@gmail.com>
2026-03-30 16:34:17 +0300

    added creation of table in postgresql

---
+++ notebooks/import_to_postgres.py
@@ -0,0 +1,49 @@
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine, text
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  
+
+DB_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_HOST = os.getenv('DB_HOST')
+TABLE_NAME = 'shark_attacks'  
+
+
+# Get data
+csv_path = r'C:/my pet projects/shark_attacks/data/processed/shark_attacks_clean.csv'
+df = pd.read_csv(csv_path)
+print(f"Loaded {len(df)} rows")
+
+# columns to lower plus no spaces/dashes
+df.columns = df.columns.str.lower().str.replace(' ', '_').str.replace('-', '_')
+print("Column names:", df.columns.tolist())
+
+# Create data base if doesnt exist
+conn_string_system = f'postgresql://postgres:{DB_PASSWORD}@localhost:5432/postgres'
+engine_system = create_engine(conn_string_system)
+
+try:
+    with engine_system.connect() as conn:
+        conn.execute(text("COMMIT"))
+        conn.execute(text(f"CREATE DATABASE {DB_NAME}"))
+        print(f"Database '{DB_NAME}' created")
+except Exception as e:
+    print(f"Database probably exists: {e}")
+
+# Connect database
+conn_string = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}'
+engine = create_engine(conn_string)
+
+# Import data
+df.to_sql(TABLE_NAME, engine, if_exists='replace', index=False)
+print(f"Data imported successfully into '{TABLE_NAME}'!")
+
+#test if worked 
+
+with engine.connect() as conn:
+    result = conn.execute(text(f"SELECT COUNT(*) FROM {TABLE_NAME}"))
+    print(f"Rows in PostgreSQL: {result.fetchone()[0]}")
